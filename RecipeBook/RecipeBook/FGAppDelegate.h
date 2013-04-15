fd57ff6c9014cb3658ad661d6a32af25c96a09cd
Le Quoc Viet <vietlq85@gmail.com>
2013-04-16 07:24:18 +0800

    Revert "blah"
    This reverts commit 395e8612280e0566e4160fd7e1d597f5a20f9c3e.

---
+++ RecipeBook/RecipeBook/FGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FGAppDelegate.h
+//  RecipeBook
+//
+//  Created by Le Quoc Viet on 16/4/13.
+//  Copyright (c) 2013 Le Quoc Viet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FGViewController;
+
+@interface FGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FGViewController *viewController;
+
+@end
