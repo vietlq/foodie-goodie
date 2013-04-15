235cc6868d0ff96b33a6a1aa6d6aa74b9d65a3fb
Le Quoc Viet <vietlq85@gmail.com>
2013-04-16 07:35:40 +0800

    Added RecipeBook with StoryBoard

---
+++ RecipeBook/RecipeBook/FGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FGAppDelegate.h
+//  RecipeBook
+//
+//  Created by Le Quoc Viet on 16/4/13.
+//  Copyright (c) 2013 Le Quoc Viet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RecipeBook/RecipeBook/FGViewController.h
@@ -0,0 +1,13 @@
+//
+//  FGViewController.h
+//  RecipeBook
+//
+//  Created by Le Quoc Viet on 16/4/13.
+//  Copyright (c) 2013 Le Quoc Viet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
