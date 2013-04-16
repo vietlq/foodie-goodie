5218ea309a75a4298778fd707fc855390fa0192d
Le Quoc Viet <vietlq85@gmail.com>
2013-04-17 05:30:45 +0800

    Runnable About page

--- RecipeBook/RecipeBook/AboutViewController.m
+++ RecipeBook/RecipeBook/AboutViewController.m
@@ -29,6 +29,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    NSURL *urlAbout = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"about.html" ofType:nil]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:urlAbout];
+    [webView loadRequest:request];
 }
 
 - (void)didReceiveMemoryWarning
