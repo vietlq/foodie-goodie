29d23d50647cf802b998447e455455d803543911
Le Quoc Viet <vietlq85@gmail.com>
2013-04-16 08:48:15 +0800

    Added property cells and now upon clicking them, a new view is shown

--- RecipeBook/RecipeBook/FGViewController.m
+++ RecipeBook/RecipeBook/FGViewController.m
@@ -15,18 +15,56 @@
 @implementation FGViewController
 {
     NSMutableArray *recipes;
+    NSString *foodMenuPath;
+}
+
+- (void)loadView
+{
+    [super loadView];
+    
+    foodMenuPath = [[NSBundle mainBundle] pathForResource:@"food-menu" ofType:@"plist"];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    recipes = [[NSMutableArray alloc] initWithContentsOfFile:foodMenuPath];
+    NSLog(@"Number of recipes: %d", [recipes count]);
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *RecipeCellIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell;
+    
+    // This is a critical piece
+    cell = [tableView dequeueReusableCellWithIdentifier:RecipeCellIdentifier];
+    
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:RecipeCellIdentifier];   
+    }
+    
+    NSDictionary * dictItem = [NSDictionary dictionaryWithDictionary:[recipes objectAtIndex:indexPath.row]];
+    
+    cell.textLabel.text = [dictItem valueForKey:@"name"];
+    NSLog(@"cell.textLabel.text = %@", cell.textLabel.text);
+    
+    return cell;
 }
 
 @end
