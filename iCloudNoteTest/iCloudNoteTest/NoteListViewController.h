757433d47d58e9b1e1fa7f142ec573d09ebef160
Le Quoc Viet <vietlq85@gmail.com>
2013-04-23 08:39:20 +0800

    Added view controllers NodeList & AddNote

---
+++ iCloudNoteTest/iCloudNoteTest/NoteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NoteListViewController.h
+//  iCloudNoteTest
+//
+//  Created by Le Quoc Viet on 23/4/13.
+//  Copyright (c) 2013 Le Quoc Viet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NoteListViewController : UITableViewController
+
+@end
